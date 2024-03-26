6f6cfb4186158341fb85b87fa8720bb158eb6dc6
rafaulgoni <rafaulgoni787898@gmail.com>
2024-03-26 14:12:52 +0600

    complete read section

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { FaRegStar } from "react-icons/fa";
+const BookCard = ({ home }) => {
+
+    const {image, bookName, author, category, rating} = home;
+    return (
+        <Link to='/details'>
+            <div className="card bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={image} alt="Shoes" className="rounded-xl w-60 h-80" />
+                </figure>
+                <div className="card-body">
+                    <div className=" flex justify-around">
+                        <button className=" text-[#23BE0A] p-1">Young Adult</button>
+                        <button className=" text-[#23BE0A] p-1">Identity</button>
+                    </div>
+                    <h2 className="card-title">{bookName}</h2>
+                    <p className="font-family">By: {author}</p>
+                    <hr className="border-dashed"/>
+                    <div className="flex justify-between">
+                        <button className="font-family">{category}</button>
+                        <button className="flex justify-center items-center font-family">{rating}<FaRegStar></FaRegStar></button>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default BookCard;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
-
+import { Link, useLoaderData } from "react-router-dom";
+import BookCard from "../BookCard";
 
 const Home = () => {
+
+    const home = useLoaderData()
+    
     return (
         <section>
             <div>
@@ -19,22 +22,13 @@ const Home = () => {
             <div className=" text-center m-10">
                 <h1 className="text-4xl font-extrabold">Books</h1>
             </div>
-            <Link to='/details'>
-                <div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Link>
+            <div className=" grid grid-cols-1 lg:grid-cols-3 lg:gap-8 mb-10 space-y-5 lg:space-y-0">
+                {
+                    home.map( home =>(
+                        <BookCard key={home.bookId} home={home}></BookCard>
+                    ))
+                }
+            </div>
         </section>
     );
 };
